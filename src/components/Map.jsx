e651f550b6823de44371845538f9bf6661cfde2e
Arshad Beg <arshad.beg@jalantechnologies.com>
2024-10-16 22:03:51 +0530

    feat: Include Map with leaflet library

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles/Map.module.css'
+import {MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+ 
+} from 'react-leaflet'
+import { useCities } from '../contexts/CitiesContext'
+
 
 const Map = () => {
+
+  const {cities} = useCities();
+
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   return (
-    <div className={styles.mapContainer}>Map</div>
+    <div className={styles.mapContainer}>
+      
+      <MapContainer
+        center={mapPosition}
+        zoom={6}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+
+      </MapContainer>
+    </div>
   )
 }
 
